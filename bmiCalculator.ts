e2692fe6788852acb073e349545f2715382de9f5
Reinout Schols <reinoutschols@hotmail.com>
2024-02-20 14:05:43 +0100

    completed exercise 9.5 WebBMI

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,7 +1,5 @@
 const calculateBmi = (height: number, weight: number) => {
-    console.log(height)
     let bmi = weight / (height / 100 * height / 100); 
-    console.log(bmi)
     if (bmi < 18.5) {
         return "Underweight";
     }
@@ -17,3 +15,5 @@ let height: number = Number(process.argv[2])
 let weight: number = Number(process.argv[3])
 
 console.log(`You are ${calculateBmi(height, weight)}`)
+
+export  { calculateBmi }
--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
    res.send("Hello Full Stack!");
 });
 
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    if (isNaN(height) || isNaN(weight) || !req.query.height || !req.query.weight) {
+        return res.status(400).send({
+            error: "malformatted parameters"
+        });
+    }    
+    const bmi = calculateBmi(height, weight);
+    return res.send({
+        weight,
+        height, 
+        bmi
+    });
+});
 const PORT = 3003;
 
 app.listen(PORT, () => {
