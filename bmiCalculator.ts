2b127aa7fcbe9af3d5b44e95265ae087c53b65d6
Reinout Schols <reinoutschols@hotmail.com>
2024-02-21 09:12:09 +0100

    completed exercise 9.6 Eslint.  Made it work with Prettier formatting on save.

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,19 +1,19 @@
 const calculateBmi = (height: number, weight: number) => {
-    let bmi = weight / (height / 100 * height / 100); 
-    if (bmi < 18.5) {
-        return "Underweight";
-    }
-    if (bmi < 25) {
-        return "Normal (healthy weight)";
-    }
-    if (bmi < 30) {
-        return "Overweight";
-    }
-    return "Obese";
-}
-let height: number = Number(process.argv[2])
-let weight: number = Number(process.argv[3])
+  const bmi = weight / (((height / 100) * height) / 100);
+  if (bmi < 18.5) {
+    return "Underweight";
+  }
+  if (bmi < 25) {
+    return "Normal (healthy weight)";
+  }
+  if (bmi < 30) {
+    return "Overweight";
+  }
+  return "Obese";
+};
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
 
-console.log(`You are ${calculateBmi(height, weight)}`)
+console.log(`You are ${calculateBmi(height, weight)}`);
 
-export  { calculateBmi }
+export { calculateBmi };
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,73 +1,77 @@
 interface Result {
-    periodLength: number;
-    trainingDays: number;
-    success: boolean;
-    rating: number;
-    ratingDescription: string;
-    target: number;
-    average: number; 
-  }
-  interface Input {
-    exerciseNums: number[];
-    target: number;
-    
-  }
-
-  const parseArguments = (args: string[]): Input => {
-    const exerciseNums = args.slice(3).map(Number);
-    const target = Number(args[2]);
-    if ( exerciseNums.length < 3) {
-        throw new Error('Input more than 3 days. ');
-    }
-    if (!isNaN(target) && exerciseNums.every(num => !isNaN(num))) {
-        return { exerciseNums, target };
-    }  else {
-        throw new Error('One or more of the provided values is not a number!');
-    }
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+interface Input {
+  exerciseNums: number[];
+  target: number;
 }
 
-const calculateRating = (average: number, target: number): number => {
-    const percentage = (average / target) * 100;
+const parseArguments = (args: string[]): Input => {
+  const exerciseNums = args.slice(3).map(Number);
+  const target = Number(args[2]);
+  if (exerciseNums.length < 3) {
+    throw new Error("Input more than 3 days. ");
+  }
+  if (!isNaN(target) && exerciseNums.every((num) => !isNaN(num))) {
+    return { exerciseNums, target };
+  } else {
+    throw new Error("One or more of the provided values is not a number!");
+  }
+};
 
-    if (percentage >= 100) {
-        return 3;
-    } else if (percentage >= 80) {
-        return 2;
-    } else {
-        return 1;
-    }
+const calculateRating = (average: number, target: number): number => {
+  const percentage = (average / target) * 100;
+
+  if (percentage >= 100) {
+    return 3;
+  } else if (percentage >= 80) {
+    return 2;
+  } else {
+    return 1;
+  }
 };
 
 const exerciseCalculator = (values: number[], target: number) => {
-    let periodLength = values.length
-    let trainingDays = values.filter((v) => v !== 0).length
-    let average = (values.reduce((accumulator, currentValue) => accumulator + currentValue, 0)) / periodLength;
-    const rating = calculateRating(average, target);
-    const success = rating > 2;
+  const periodLength = values.length;
+  const trainingDays = values.filter((v) => v !== 0).length;
+  const average =
+    values.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    ) / periodLength;
+  const rating = calculateRating(average, target);
+  const success = rating > 2;
 
-    const ratingDescriptions = [
-        "You did not reach the exercise target, you can do better!",
-        "You almost reached the exercise target, not bad!",
-        "You exceeded the exercise target, great job!"
-    ];
-    const ratingDescription = ratingDescriptions[rating - 1];
+  const ratingDescriptions = [
+    "You did not reach the exercise target, you can do better!",
+    "You almost reached the exercise target, not bad!",
+    "You exceeded the exercise target, great job!",
+  ];
+  const ratingDescription = ratingDescriptions[rating - 1];
 
-    const result: Result = {
-        periodLength,
-        trainingDays,
-        success,
-        rating,
-        ratingDescription,
-        target,
-        average
-    };
+  const result: Result = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
 
-    return result;
-}
+  return result;
+};
 
 try {
-    const { exerciseNums, target } = parseArguments(process.argv);
-    console.log(exerciseCalculator(exerciseNums, target));
+  const { exerciseNums, target } = parseArguments(process.argv);
+  console.log(exerciseCalculator(exerciseNums, target));
 } catch (error) {
-    console.error(error.message);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  console.error(error.message);
 }
